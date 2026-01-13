0a44f4df3cd18624eb0ad108e89964e17ea13b54
maxxandres <mmaure19@alumnos.utalca.cl>
2026-01-13 14:30:14 -0300

    creacion de repo e instalacion de vue

---
+++ src/components/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Welcome to the Home Component</h1>
+  <p>This is the home page of the application.</p>  
+   <p>Hola</p>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
